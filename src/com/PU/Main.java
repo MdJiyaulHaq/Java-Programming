a39bf0a589ef465371daec26afa4b529664e83dd
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-11-16 14:58:22 +0545

    Initial commit

---
+++ src/com/PU/Main.java
@@ -0,0 +1,8 @@
+package com.PU;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("We surely belong to Allah and to him we shall return.");
+    }
+}
