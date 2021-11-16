303b9aa21004d21bb99c0708604f19409858b9f1
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-11-16 15:49:36 +0545

    updated

---
+++ src/com/PU/comment.java
@@ -0,0 +1,11 @@
+package com.PU;
+
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("comments in java");
+        // single line comment
+        /* multi line
+        comment
+         */
+    }
+}
---
+++ src/com/PU/data_types.java
@@ -0,0 +1,24 @@
+package com.PU;
+
+public class data_types<name> {
+    public static void main(String[] args) {
+        System.out.println("variables in java");
+        // variables are same as like that of cpp
+        String name = "Md";
+        char character = 'a';
+        int integer= 10;
+        float flt= (float) 3.2;
+        boolean bl = false;
+        System.out.println(name);
+        System.out.println(character);
+        System.out.println(integer);
+        System.out.println(flt);
+        System.out.println(false);
+
+        // if you don't want an integer value to change
+        final int x=2;
+        // x=4  changing this value will throw an error
+
+    }
+
+}
