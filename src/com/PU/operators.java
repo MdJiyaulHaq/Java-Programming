fb6594d3f7cf1191991102aa165f9acf81f01bb3
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-11-23 21:41:17 +0545

    updated

---
+++ src/com/PU/operators.java
@@ -0,0 +1,11 @@
+package com.PU;
+
+public class operators {
+    public static void main(String[] args) {
+        int s1 = 3 + 3;
+        int s2 = 4;
+        int s3 = 8 + s1 + s2;
+        System.out.println(s3);
+
+    }
+}
