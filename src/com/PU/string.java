8f52d3021bed967562534c19bb6e7d7c40a779fa
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-11-23 22:02:41 +0545

    STRING

---
+++ src/com/PU/string.java
@@ -0,0 +1,18 @@
+package com.PU;
+
+public class string {
+    public static void main(String[] args) {
+        System.out.println("Strings in java.");
+        String str = "Muhammad peace be upon him.";
+        String str2 = "He is the Prophet of Allah.";
+        System.out.println("The length of the above string is :" + str.length());
+
+        // finding character in the string
+        System.out.println(str.indexOf("be"));
+
+        // concatenate string
+        System.out.println(str + str2);
+
+
+    }
+}
