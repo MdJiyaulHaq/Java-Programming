a65c32e1cfd3bb89d38e8dfe4b2ce48898ddfb39
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-12-03 22:05:27 +0545

    methods in java

---
+++ src/com/PU/Methods.java
@@ -0,0 +1,14 @@
+package com.PU;
+
+public class Methods {
+    static void myMethod()
+    {
+        System.out.println("i am a method named myMethod.");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod(); // a method can be called several times
+    }
+}
