24259a65455a2f8db59ccf13c0794ad87e4cb141
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2021-12-21 20:42:11 +0545

    volume of cube

---
+++ src/com/PU/BankAccount.java
@@ -0,0 +1,54 @@
+package com.PU;
+
+public class BankAccount {
+    int account_number;
+    String name;
+    float balance;
+
+    // account creation function
+    void put_account_info(int account_number, String name, float balance){
+        this.account_number=account_number;
+        this.name=name;
+        this.balance=balance;
+    }
+
+    // account information
+    void Account_info(){
+        System.out.println(account_number);
+        System.out.println(name);
+        System.out.println(balance);
+    }
+
+    // deposit amount
+    void deposit(float amount){
+        balance = balance+amount;
+        System.out.println("Balance Deposited:");
+    }
+
+    // withdraw amount
+    void withdraw(float amount){
+        if (balance==0 || balance<amount){
+            System.out.println("Insufficient balance:");
+        }
+        else {
+            balance = balance-amount;
+        }
+    }
+
+    // balance inquiry
+    void balance_inquiry(){
+        System.out.println("Your balance is :"+balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount A1 = new BankAccount();
+        A1.put_account_info(78692, "Md", 78692);
+        A1.Account_info();
+        A1.balance_inquiry();
+        A1.deposit(45);
+        A1.balance_inquiry();
+        A1.withdraw(46);
+        A1.balance_inquiry();
+    }
+
+}
