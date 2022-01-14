8162d19f0808dcb406f13ec05e91bdb93151f641
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-01-14 21:40:16 +0545

    throw

--- src/com/KU/ThrowKeyword.java
+++ src/com/KU/ThrowKeyword.java
@@ -2,6 +2,6 @@ package com.KU;
 
 public class ThrowKeyword {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
     }
 }
