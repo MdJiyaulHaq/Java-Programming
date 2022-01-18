d707d708ef17d7b8ac6de3317b8f9ddfb2574b7f
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-01-18 17:41:27 +0545

    try catch exception handling

---
+++ src/com/Advance/TryCatch2.java
@@ -0,0 +1,17 @@
+package com.Advance;
+
+public class TryCatch2 {
+    public static void main(String[] args) {
+        int a = 3, b = 0;
+        try {
+            int result = a / b;
+        }
+        // handling the exception using Exception class
+        catch (Exception e) {
+            System.out.println("Division by zero is not allowed.");
+            // custom message can also be displayed.
+            System.out.println(e);
+        }
+        System.out.println("rest of the code.");
+    }
+}
---
+++ src/com/Advance/TryCatch4.java
@@ -0,0 +1,16 @@
+
+package com.Advance;
+
+public class TryCatch4 {
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 8, 1};
+        try {
+            System.out.println(arr[6]);
+        }
+        // exception handling for arrays
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+        System.out.println("rest of the code.");
+    }
+}
