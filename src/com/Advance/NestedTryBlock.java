410585fa7a809ee3f2892341f9e329c134f978f9
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-01-18 21:27:41 +0545

    nested try block

---
+++ src/com/Advance/NestedTryBlock.java
@@ -0,0 +1,25 @@
+package com.Advance;
+
+public class NestedTryBlock {
+    public static void main(String[] args) {
+        int a = 4, b = 0;
+        int[] arr = new int[5];
+        String s = null;
+        try {
+            try {
+                int result = a / b;
+            } catch (ArithmeticException e1) {
+                System.out.println(e1);
+            }
+            try {
+                arr[6] = 3;
+            } catch (ArrayIndexOutOfBoundsException e2) {
+                System.out.println(e2);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // also, if the catch block for exception is not defined then,
+        //the java will automatically invoke one.
+    }
+}
