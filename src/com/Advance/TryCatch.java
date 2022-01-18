985894cc08b82801fc26485c820f575ffcd81a36
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-01-18 17:21:43 +0545

    try catch exception handling

--- src/com/Advance/TryCatch.java
+++ src/com/Advance/TryCatch.java
@@ -6,6 +6,8 @@ public class TryCatch {
         int b = 0;
         try {
             int result = a / b;
+            System.out.println("some more codes.");
+            // codes that doesn't throw error shouldn't be put in try block
         } catch (ArithmeticException e) {
             System.out.println(e);
             System.out.println("the remaining code");
