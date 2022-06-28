31443253e57dafb2fbf01e8ea2bd363d192e245f
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-06-28 17:30:42 +0545

    created and written to file

--- src/com/Advance/FileHandlingAndIO/CreateAFile.java
+++ src/com/Advance/FileHandlingAndIO/CreateAFile.java
@@ -1,4 +1,11 @@
 package com.Advance.FileHandlingAndIO;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CreateAFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("example.txt");
+        file.createNewFile();
+    }
 }
---
+++ src/com/Advance/FileHandlingAndIO/WriteToFile.java
@@ -0,0 +1,19 @@
+package com.Advance.FileHandlingAndIO;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter fileWriter = new FileWriter("example.txt");
+            fileWriter.write("We surely belong to Allah and to him we shall return.");
+            fileWriter.write("\nBeing with the name of Allah who is most mercyful.");
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
