b4e05de29ebbed6bc0f976d9a336ec56ca4d0753
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-07-02 21:01:59 +0545

    url class

--- src/com/Advance/Networking/URLclass.java
+++ src/com/Advance/Networking/URLclass.java
@@ -5,14 +5,14 @@ import java.net.URL;
 public class URLclass {
     public static void main(String[] args) {
         try {
-            URL url =new URL("https://codekavya.com/");
-            System.out.println("protocol"+ url.getProtocol());
-            System.out.println("authority"+ url.getAuthority());
-            System.out.println("port"+ url.getPort());
-            System.out.println("host"+ url.getHost());
-            System.out.println("file"+ url.getFile());
-            System.out.println("path"+ url.getPath());
-        }catch (Exception e) {
+            URL url = new URL("https://codekavya.com/");
+            System.out.println("protocol" + url.getProtocol());
+            System.out.println("authority" + url.getAuthority());
+            System.out.println("port" + url.getPort());
+            System.out.println("host" + url.getHost());
+            System.out.println("file" + url.getFile());
+            System.out.println("path" + url.getPath());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
