e324b938dd035958e5871edd0356f257f1e08836
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-07-02 21:01:42 +0545

    url class

---
+++ src/com/Advance/Networking/URLclass.java
@@ -0,0 +1,19 @@
+package com.Advance.Networking;
+
+import java.net.URL;
+
+public class URLclass {
+    public static void main(String[] args) {
+        try {
+            URL url =new URL("https://codekavya.com/");
+            System.out.println("protocol"+ url.getProtocol());
+            System.out.println("authority"+ url.getAuthority());
+            System.out.println("port"+ url.getPort());
+            System.out.println("host"+ url.getHost());
+            System.out.println("file"+ url.getFile());
+            System.out.println("path"+ url.getPath());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
