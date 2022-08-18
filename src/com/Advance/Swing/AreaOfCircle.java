13ffa43c821e344e613217f106efde852fa853a2
Md Zia-ul <69068755+Zia-ul-haq@users.noreply.github.com>
2022-08-18 13:05:49 +0545

    textfield added

--- src/com/Advance/Swing/AreaOfCircle.java
+++ src/com/Advance/Swing/AreaOfCircle.java
@@ -9,16 +9,19 @@ public class AreaOfCircle extends JFrame implements ActionListener {
     JFrame f = new JFrame("Area of circle");
     JButton b = new JButton("Area");
     JTextField t = new JTextField();
+    JTextField t2 = new JTextField();
     JLabel l = new JLabel("Result is : ");
 
     AreaOfCircle() {
         t.setBounds(33, 33, 66, 22);
         b.setBounds(33, 77, 66, 22);
         l.setBounds(33, 112, 66, 22);
-        f.setSize(333, 333);
+        t2.setBounds(99, 112, 66, 22);
+        f.setSize(444, 444);
         f.add(b);
         f.add(t);
         f.add(l);
+        f.add(t2);
         f.setLayout(null);
         f.setVisible(true);
         b.addActionListener(this);
@@ -32,7 +35,7 @@ public class AreaOfCircle extends JFrame implements ActionListener {
         if (ae.getSource() == b) {
             int n = Integer.parseInt(t.getText());
             int area = (int) (n * n * 3.14159);
-            l.setText("Result is:" + area);
+            t2.setText("" + area);
         }
     }
 }
